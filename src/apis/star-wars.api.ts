07cc5b1fd80bbae861d73c5b25b63f680c458b4b
lukasrafael1407 <lucasrafaelbarbosa@gmail.com>
2020-06-20 11:46:53 -0300

    Atualização

--- src/apis/star-wars.api.ts
+++ src/apis/star-wars.api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const baseURL = 'https://star-wars-api-unifacef.herokuapp.com';
-//const baseURL = process.env.REACT_APP_STAR_WARS_BASE_URL
+//const baseURL = 'https://star-wars-api-unifacef.herokuapp.com';
+const baseURL = process.env.REACT_APP_STAR_WARS_BASE_URL
 
 export const getFilms =() =>{
   return axios.request({ baseURL, url: 'films' })
